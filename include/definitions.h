687d0d3ca9175e2c9c9555786a7cae3c18235ebd
Besnik Nuro <besnn@konobook.local>
2023-05-20 19:00:48 +0200

    changed project structure; added CLI11 and Dear ImGui

--- include/definitions.h
+++ include/definitions.h
@@ -5,6 +5,20 @@
 #ifndef NAMEPLACEHOLDER_DEFINITIONS_H
 #define NAMEPLACEHOLDER_DEFINITIONS_H
 
+enum OS {
+    NT,
+    Linux,
+    MacOS
+};
+
+#ifdef _WIN32
+const OS os = NT;
+#elif __linux__
+const OS os = Linux;
+#elif __APPLE__
+const OS os = MacOS;
+#endif
+
 #define INVERTEDCOLORING 1
 
 #endif //NAMEPLACEHOLDER_DEFINITIONS_H
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <ncurses.h>
+
+#include "../include/definitions.h"
+
+int main() {
+
+
+}
+
